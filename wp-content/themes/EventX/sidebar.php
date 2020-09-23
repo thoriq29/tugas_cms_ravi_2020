30a58011e32755c2f731a51f0468135ca5bae0db
Rafi Septian <rafiseptian90@gmail.com>
2020-09-23 22:12:13 +0700

    add sidebar

--- wp-content/themes/EventX/sidebar.php
+++ wp-content/themes/EventX/sidebar.php
@@ -1,6 +1,21 @@
 <aside id="sidebar">
     <div class="content">
-        <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores necessitatibus odio porro sint ut. Animi aperiam cupiditate deserunt, dolore fuga, fugit in libero necessitatibus, non numquam quis quisquam reprehenderit voluptas!</h1>
+        <h3 class="text-center">
+            Sponsors
+        </h3>
+        <?php $sponsors = get_posts([
+                'post_type' => 'sponsor',
+                'numberposts' => -1
+        ]); ?>
+
+        <div class="sponsor-wrapper">
+            <?php foreach($sponsors as $sponsor) : ?>
+                <div class="sponsor">
+                    <img src="<?= get_the_post_thumbnail($sponsor->ID) ?>
+                <h4 class="text-center"><?= $sponsor->post_title ?></h4>
+                </div>
+            <?php endforeach; ?>
+        </div>
     </div>
 
     <div class="toggler">
