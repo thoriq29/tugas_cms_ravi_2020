7c98c8b4c9cd2a9486b72f2d5f5073b4edeafb3d
Rafi Septian <rafiseptian90@gmail.com>
2020-09-23 15:47:54 +0700

    save date section

--- wp-content/themes/EventX/index.php
+++ wp-content/themes/EventX/index.php
@@ -1,5 +1,10 @@
 <?php get_header(); ?>
 
+<?php $customize = get_posts([
+    'post_type' => 'customize',
+    'numberposts' => 1
+]); ?>
+
 <main id="main">
     <section id="news">
         <div class="container">
@@ -27,11 +32,28 @@
                             <p>
                                 <?= substr($news->post_content, 0, 150) ?>...
                             </p>
-                            <a href="" class="btn btn-primary full-width">Read more</a>
+                            <a href="" class="btn btn-secondary full-width">Read more</a>
                         </div>
                     </div>
                 <?php endforeach; ?>
             </div>
+        </div>
+    </section>
+
+    <section id="save_date">
+        <div class="container">
+            <h2>Let us remind you when the event will begin</h2>
+            <h4>Event will begin at</h4>
+            <?php foreach($customize as $custom) : ?>
+                <span class="d-block">
+                    <?php
+                        $now = new DateTime();
+                        $event_date = new DateTime(get_field('event_date', $custom->ID));
+
+                        echo $event_date->format('Y');
+                    ?>
+                </span>
+            <?php endforeach; ?>
         </div>
     </section>
 </main>
