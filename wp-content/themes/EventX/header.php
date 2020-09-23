567415b38e4642070b9471d3f7a9a19737f4f416
Rafi Septian <rafiseptian90@gmail.com>
2020-09-23 13:57:53 +0700

    create header

--- wp-content/themes/EventX/header.php
+++ wp-content/themes/EventX/header.php
@@ -11,5 +11,27 @@
 <body>
 
 <header id="header">
+    <div id="jssor_1" style="width:100vw;height:100vh;overflow:hidden;visibility:hidden;background-color:#000000;">
+        <!-- Loading Screen -->
+        <div data-u="loading" style="position:absolute;top:0px;left:0px;background-color:rgba(0,0,0,0.7);">
+            <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
+            <div style="position:absolute;display:block;background:url('img/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div>
+        </div>
+        <div data-u="slides" class="slides" style="cursor:default;width:100vw;height:100vh;overflow:hidden;">
+            <?php $artists = get_posts([
+                    'post_type' => 'artist',
+                    'numberposts' => -1
+            ]) ?>
 
+            <?php foreach($artists as $artist) : ?>
+                <div>
+                    <img data-u="image" class="img-slider" src="<?= get_the_post_thumbnail($artist->ID) ?>" />
+                    <div data-u="thumb">
+                        <img class="i" src="<?= get_the_post_thumbnail($artist->ID) ?>" />
+                        <audio id="music-<?= $artist->ID ?>" src="<?= get_field('mp3', $artist->ID) ?>"></audio>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 </header>
