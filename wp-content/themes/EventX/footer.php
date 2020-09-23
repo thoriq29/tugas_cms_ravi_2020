0486f27bcb240a4f8c03a00bd283cab228a62187
Rafi Septian <rafiseptian90@gmail.com>
2020-09-23 16:39:48 +0700

    done footer

--- wp-content/themes/EventX/footer.php
+++ wp-content/themes/EventX/footer.php
@@ -1,5 +1,42 @@
 <footer id="footer">
+    <div class="container">
+        <div class="content">
+            <div class="subscribe">
+                <h4 class="text-center">
+                    Subscribe Us
+                </h4>
 
+                <form action="" method="post">
+                    <input type="email" class="custom-input" placeholder="example@example.com">
+                    <button type="submit" class="btn btn-secondary">
+                        Subscribe
+                    </button>
+                </form>
+            </div>
+
+            <div class="contact-us text-center">
+                <h4>
+                    Contact Us
+                </h4>
+                <span class="d-block">
+                    our.eventxcorp@event.com
+                </span>
+            </div>
+
+            <div class="follow text-center">
+                <h4>
+                    Follow Us
+                </h4>
+                <span class="d-block">
+                    our.eventxcorp@event.com
+                </span>
+            </div>
+        </div>
+
+        <span class="d-block text-center copyright">
+            Copyright &copy; rshme - <?= date('Y') ?>
+        </span>
+    </div>
 </footer>
 <?php wp_footer(); ?>
 </body>
