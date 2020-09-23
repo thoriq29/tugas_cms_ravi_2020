50e5da7204fa4fe1dcaaff7355860f6a4ec222a9
Rafi Septian <rafiseptian90@gmail.com>
2020-09-23 13:42:27 +0700

    modif css

--- wp-content/themes/EventX/index.php
+++ wp-content/themes/EventX/index.php
@@ -3,9 +3,14 @@
 <main id="main">
     <section id="news">
         <div class="container">
-            <h3 class="section-header">
-                News
-            </h3>
+           <div class="header text-center">
+               <h3 class="section-header">
+                   News
+               </h3>
+               <span>
+                   All information about event in here
+               </span>
+           </div>
 
             <div class="content">
                 <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam corporis deleniti ducimus earum, est facere, facilis id in laudantium nemo nihil nisi non porro quo repellendus, sed similique sit.</h3>
--- wp-content/themes/EventX/js/main.js
+++ wp-content/themes/EventX/js/main.js
@@ -1,5 +1,46 @@
 const $ = jQuery;
 
 $(document).ready(function(){
+    // jssor init
+    jssor_1_slider_init = function() {
+
+        var jssor_1_options = {
+            $AutoPlay: true,
+            $ArrowNavigatorOptions: {
+                $Class: $JssorArrowNavigator$
+            },
+            $ThumbnailNavigatorOptions: {
+                $Class: $JssorThumbnailNavigator$,
+                $Cols: 4,
+                $SpacingX: 4,
+                $SpacingY: 4,
+                $Orientation: 2,
+                $Align: 0
+            }
+        };
+
+        var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+        /*responsive code begin*/
+        /*remove responsive code if you don't want the slider scales while window resizing*/
+        function ScaleSlider() {
+            var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+            if (refSize) {
+                refSize = Math.min(refSize, 810);
+                jssor_1_slider.$ScaleWidth(refSize);
+            }
+            else {
+                window.setTimeout(ScaleSlider, 30);
+            }
+        }
+        ScaleSlider();
+        $Jssor$.$AddEvent(window, "load", ScaleSlider);
+        $Jssor$.$AddEvent(window, "resize", ScaleSlider);
+        $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+        /*responsive code end*/
+    };
+    jssor_1_slider_init();
+
+    // write code here
 
 })
