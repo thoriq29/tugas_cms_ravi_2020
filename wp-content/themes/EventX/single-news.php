3f2a95f99cc0b7c14c7157ee3639c7d2f1fe3d19
Rafi Septian <rafiseptian90@gmail.com>
2020-09-23 23:00:30 +0700

    single page done

--- wp-content/themes/EventX/single-news.php
+++ wp-content/themes/EventX/single-news.php
@@ -10,14 +10,63 @@
 </head>
 <body>
 
+<?php get_sidebar(); ?>
+
 <header id="news-header">
-    <div class="container">
-        <h1>
-            <?= the_title() ?>
-        </h1>
+    <div class="header">
+        <div class="container">
+            <h1>
+                <?= the_title() ?>
+            </h1>
+        </div>
+    </div>
+
+    <div class="content">
+        <div class="container">
+            <p>
+                <?= the_content(); ?>
+            </p>
+        </div>
     </div>
 </header>
 
-<?php wp_footer(); ?>
+<section id="news">
+    <div class="container">
+        <div class="header text-center">
+            <h3 class="section-header">
+                News
+            </h3>
+            <span>
+                Let see another news below
+            </span>
+        </div>
+
+        <div class="content">
+            <?php
+            $id = get_the_ID();
+            $data_news = get_posts([
+                'post_type' => 'news',
+                'numberposts' => 3,
+                'exclude'  => array( $id ),
+            ]) ?>
+
+            <?php foreach($data_news as $news) : ?>
+                <div class="card">
+                    <div class="card-body">
+                        <h1 class="card-title">
+                            <?= $news->post_title ?>
+                        </h1>
+                        <p>
+                            <?= substr($news->post_content, 0, 150) ?>...
+                        </p>
+                        <a href="<?= get_the_permalink($news->ID) ?>" class="btn btn-secondary full-width">Read more</a>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
 </body>
 </html>
